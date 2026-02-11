9b3f2ed4b44599454f4c486b660ea976a68eaa8e
H e n r y <office.oliver0@gmail.com>
2026-02-11 17:05:06 -0300

    Delete src/main directory

--- src/main/java/com/deliverytech/delivery/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deliverytech.delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.deliverytech.delivery.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
